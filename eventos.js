f21d91aeb8535f1f131acc9e7a74880ebab87de7
DANIEL FERNANDO PINHA <pinhadaniel23@gmail.com>
2026-04-18 22:46:55 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,10 @@
+window.eventos = {
+  "2026-04-17": [
+    {
+      "title": "Exemplo: Sábado Letivo",
+      "time": "12:35h - 18:00h",
+      "description": "Aula Normal.",
+    
+    },
+  ]
+};
